d07af02e38becb600fc1443698e8aa9674bf73df
Carlos Augusto <carlosaugustoslima@gmail.com>
2026-04-05 05:25:09 -0300

    Comit inicial do projeto MeuCronometro

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace MeuCronometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnPausar = new System.Windows.Forms.Button();
+            this.btnZerar = new System.Windows.Forms.Button();
+            this.btnVolta = new System.Windows.Forms.Button();
+            this.lstVoltas = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.Location = new System.Drawing.Point(43, 80);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(328, 59);
+            this.lblTempo.TabIndex = 0;
+            this.lblTempo.Text = "00:00:00.000";
+            this.lblTempo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTempo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnIniciar.Location = new System.Drawing.Point(53, 190);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnPausar
+            // 
+            this.btnPausar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnPausar.Location = new System.Drawing.Point(154, 190);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(75, 23);
+            this.btnPausar.TabIndex = 2;
+            this.btnPausar.Text = "Pausar";
+            this.btnPausar.UseVisualStyleBackColor = false;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnZerar
+            // 
+            this.btnZerar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnZerar.Location = new System.Drawing.Point(260, 190);
+            this.btnZerar.Name = "btnZerar";
+            this.btnZerar.Size = new System.Drawing.Size(75, 23);
+            this.btnZerar.TabIndex = 3;
+            this.btnZerar.Text = "Zerar";
+            this.btnZerar.UseVisualStyleBackColor = false;
+            this.btnZerar.Click += new System.EventHandler(this.btnZerar_Click);
+            // 
+            // btnVolta
+            // 
+            this.btnVolta.Location = new System.Drawing.Point(482, 190);
+            this.btnVolta.Name = "btnVolta";
+            this.btnVolta.Size = new System.Drawing.Size(75, 23);
+            this.btnVolta.TabIndex = 4;
+            this.btnVolta.Text = "Marcar Volta";
+            this.btnVolta.UseVisualStyleBackColor = true;
+            this.btnVolta.Click += new System.EventHandler(this.btnVolta_Click);
+            // 
+            // lstVoltas
+            // 
+            this.lstVoltas.FormattingEnabled = true;
+            this.lstVoltas.Location = new System.Drawing.Point(563, 190);
+            this.lstVoltas.Name = "lstVoltas";
+            this.lstVoltas.Size = new System.Drawing.Size(225, 238);
+            this.lstVoltas.TabIndex = 5;
+            this.lstVoltas.SelectedIndexChanged += new System.EventHandler(this.lstVoltas_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVoltas);
+            this.Controls.Add(this.btnVolta);
+            this.Controls.Add(this.btnZerar);
+            this.Controls.Add(this.btnPausar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblTempo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnPausar;
+        private System.Windows.Forms.Button btnZerar;
+        private System.Windows.Forms.Button btnVolta;
+        private System.Windows.Forms.ListBox lstVoltas;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MeuCronometro
+{
+    public partial class Form1 : Form
+    {
+        //Criando a variável principal do tipo Stopwatch
+        private Stopwatch cronometro;
+        public Form1()
+        {
+            InitializeComponent();
+            //Inicializando o Stopwatch quando o programa abre
+            cronometro = new Stopwatch();
+        }
+
+        private void btnVolta_Click(object sender, EventArgs e)
+        {
+            //Pega o tempo que está no Label agora e adiciona como um item na lista
+            if(cronometro.IsRunning)
+            {
+                int numeroDaVolta = lstVoltas.Items.Count + 1;
+                lstVoltas.Items.Add($"Volta{numeroDaVolta}: {lblTempo.Text}");
+            }
+        }
+
+        private void lstVoltas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            cronometro.Start(); //Começa a contar o tempo real
+            timer1.Start(); //Começa a atualizar a tela
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e)
+        {
+            cronometro.Stop(); //Pausa a contagem
+            timer1.Stop(); //Pausa a atualização da tela
+        }
+
+        private void btnZerar_Click(object sender, EventArgs e)
+        {
+            cronometro.Reset(); //Zera o contador interno
+            lblTempo.Text = "00:00:00.000"; //Zera a tela
+            lstVoltas.Items.Clear(); //Limpa o histórico d voltas (Opcional)
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Este método irá rodar a cada 10 milissegundos
+            TimeSpan tempo = cronometro.Elapsed;
+
+            //Formatação da exibição para Horas:Minutos:Segundos.Milissegundos
+            lblTempo.Text = string.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                tempo.Hours,
+                tempo.Minutes,
+                tempo.Seconds,
+                tempo.Milliseconds);
+        }
+    }
+}
